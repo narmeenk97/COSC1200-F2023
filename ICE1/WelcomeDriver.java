5c8991cbe67c54761856f33275e37784a64edec8
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-09-21 20:02:08 -0400

    Code demo in class

---
+++ ICE1/WelcomeDriver.java
@@ -0,0 +1,28 @@
+/***************************
+ * Name: Narmeen Khalid
+ * Course: COSC 1200
+ * Date: September 21, 2023
+ * Description: ICE #1: WelcomeDriver.java
+ ***************************/
+package ICE1;
+
+import java.util.Scanner;
+
+public class WelcomeDriver{
+
+    public static void main(String[] args){
+
+        //welcome message
+        System.out.println("Welcome to COSC 1200 - ICE 1");
+        System.out.println("My favourite quote is: Fear doesnt have to make you cruel or cowardly, fear can make you kind");
+        System.out.println("- The Doctor");
+
+        //numeric input
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter in a number between (50 to 999): ");
+        String strValue = input.nextLine();
+
+
+
+    }
+}
