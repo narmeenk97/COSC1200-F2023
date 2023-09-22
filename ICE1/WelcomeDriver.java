0ce90e4580812df1ef3fa62bab4818d0d1b44195
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-09-21 20:11:51 -0400

    Code demo in class

--- ICE1/WelcomeDriver.java
+++ ICE1/WelcomeDriver.java
@@ -14,13 +14,25 @@ public class WelcomeDriver{
 
         //welcome message
         System.out.println("Welcome to COSC 1200 - ICE 1");
-        System.out.println("My favourite quote is: Fear doesnt have to make you cruel or cowardly, fear can make you kind");
+        System.out.println("My favourite quote is: Fear doesnt have to make you cruel or cowardly, fear can make you kind.");
         System.out.println("- The Doctor");
 
         //numeric input
         Scanner input = new Scanner(System.in);
         System.out.println("Enter in a number between (50 to 999): ");
         String strValue = input.nextLine();
+
+        //convert string to double
+        double numValue = Double.parseDouble(strValue);
+
+        if (50 <= numValue && numValue <= 999) {
+            System.out.println("The number you entered is..." + numValue + " and it is within the acceptable boundaries");
+
+        } else {
+
+            System.out.println("Alert, the number you entered is..." + numValue + " and it is NOT within the acceptable" +
+                    "boundaries of (50 and 999");
+        }
 
 
 
