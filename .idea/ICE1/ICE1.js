5062259c0cfc15d2fc3a699897a58fbbfe35f72f
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-09-27 16:32:29 -0400

    Donwloaded files

---
+++ .idea/ICE1/ICE1.js
@@ -0,0 +1,15 @@
+package ice1;
+
+public class WelcomeDriver {
+
+    public static void main(string args[])
+
+        sout("Welcome to COSC 1200 - ICE 1");
+        sout("My favourite quote: For where all love is, the speaking is unnecessary,
+        "it is all, it is undying, and it is enough");
+        sout("- Diana Gabaldon")
+
+
+
+
+}
